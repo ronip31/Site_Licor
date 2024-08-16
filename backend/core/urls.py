376f674f5d06810ae276c05f79a0884e0d4fed18
RONI PEREIRA <roni.pereira31@gmail.com>
2024-08-15 23:09:41 -0300

    first Commit

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('usuarios/', UsuarioCreateView.as_view(), name='usuario-create'),
     path('usuarios/lista/', UsuarioListView.as_view(), name='usuario-lista'),
     path('usuarios/<int:pk>/', UsuarioDetailView.as_view(), name='usuario-detalhe'),
-     path('usuarios/<int:pk>/delete/', UsuarioDeleteView.as_view(), name='usuario-delete'),
+    path('usuarios/<int:pk>/delete/', UsuarioDeleteView.as_view(), name='usuario-delete'),
 ]
